662b5611f561054a55ab8b5ba3e7460d81c70d83
WinterFlw <winterflw@hansung.ac.kr>
2023-08-12 23:03:20 +0000

    error fix

--- code/magic_soragodong.py
+++ code/magic_soragodong.py
@@ -1,7 +1,9 @@
 import random
 
 def soragodong():
-    with open("magic_soragodong_answer.txt", "r") as f:
+    with open("/workspace/GPT_Discord_Bot/code/magic_soragodong_answer.txt", "r", encoding="utf-8") as f:
         random_responses = f.readlines()
         respones = random.choice(random_responses)
     return(respones)
+
+print(soragodong())
